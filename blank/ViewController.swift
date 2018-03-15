fa70ee494478b8fe4d4f7673aea80b0dd3480fbc
eno udo <enoudo7@gmail.com>
2018-03-15 00:48:45 -0400

    pressing the button swaps the label between 1 an 2

--- blank/ViewController.swift
+++ blank/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbTest: UILabel!
     @IBAction func btnTest(_ sender: Any) {
-        print("test")
+        if(lbTest.text == "1"){
+            lbTest.text = "2"
+        }else{
+            lbTest.text = "1"
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
